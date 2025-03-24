dd35605d6677ec9896bb4ec5c888d0f54a7224b7
SuperSteve9 <80017099+SuperSteve9@users.noreply.github.com>
2025-03-24 01:34:19 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,25 +1,23 @@
-echo "Checking for updates"
-if exists mySFHS/ (
-  cd mySFHS
-) else (
+@echo off
+echo Checking for updates...
+
+if not exist mySFHS (
   mkdir mySFHS
-  cd mySFHS
 )
+cd mySFHS
 
-curl "https://github.com/SuperSteve9/MySFHSplus/new/main/content.js" --output content.js
-curl "https://github.com/SuperSteve9/MySFHSplus/new/main/jquery-3.7.1.min.js" --output jquery-3.7.1.min.js
-curl "https://github.com/SuperSteve9/MySFHSplus/new/main/manigest.json" --output manifest.json
-curl "https://github.com/SuperSteve9/MySFHSplus/new/main/popup.html" --output popup.html
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/content.js" --output content.js
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/jquery-3.7.1.min.js" --output jquery-3.7.1.min.js
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/manifest.json" --output manifest.json
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/popup.html" --output popup.html
 
-if exists icons/ (
-  cd icons
-) else (
+if not exist icons (
   mkdir icons
-  cd icons
 )
-curl "https://github.com/SuperSteve9/MySFHSplus/new/main/icons/128.png" --output 128.png
+cd icons
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/icons/128.png" --output 128.png
 
 cd ..
 cd ..
 
-curl "https://github.com/SuperSteve9/MySFHSplus/new/main/run.bat" --output run.bat
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/run.bat" --output run.bat
