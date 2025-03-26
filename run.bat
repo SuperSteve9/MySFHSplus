fa59333a6ae1695326eed4921842e81dd269a29a
SuperSteve9 <80017099+SuperSteve9@users.noreply.github.com>
2025-03-25 23:51:53 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,9 +7,14 @@ if not exist mySFHS (
 cd mySFHS
 
 curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/content.js" --output content.js
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/gamecode.js" --output gamecode.js
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/init-buffers.js" --output init-buffers.js
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/draw-scene.js" --output draw-scene.js
 curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/jquery-3.7.1.min.js" --output jquery-3.7.1.min.js
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/gl-matrix-min.js" --output gl-matrix-min.js
 curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/manifest.json" --output manifest.json
 curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/popup.html" --output popup.html
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/game.html" --output game.html
 
 if not exist icons (
   mkdir icons
