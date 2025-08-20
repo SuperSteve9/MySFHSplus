f2949e5aec18928b4cbe57e8dbb216b80f17c4f8
SuperSteve9 <bgepictest@gmail.com>
2025-08-20 00:35:38 -0500

    remade
    remade the core functions

--- content.js
+++ content.js
@@ -1,446 +1,191 @@
-console.log('%cGet out of here and focus in class! ', 'color: #FF0000');
+// const body = document.querySelector("body");
+// body.style.setProperty("background-color", "#252525");
+const observer = new MutationObserver(() => {
+    // deep ass element to wait for observer
+    const target = getElementFromXPath('//*[@id="coursesContainer"]/div[9]/div[4]/h3', document);
 
-(function () {
-  let observer;
-  let gradeElems = [];
-  let weightedElems = [];
-  let baseTile;
-  let gpas = [];
-  let grades = [];
-  let classes = [];
+    if (target) {
+        observer.disconnect();
+        createBanner("MYSFHS+ DEVELOPMENT VERSION 2.5.10 DO NOT DISTRIBUTE", document);
+        const row = getElementFromXPath('//*[@id="site-main"]/div/div/div/div[3]', document);
+        const conductDIV = getElementFromXPath('//*[@id="conduct"]', document);
 
-  function main() {
-    changeColorTopBar(localStorage.getItem('topCol'));
-    changeColorMiddleBar(localStorage.getItem('midCol'));
-    changeColorLowerBar(localStorage.getItem('lowCol'));
-    changeColorBody(localStorage.getItem('bodyCol'));
-    changeColorTileTitle(localStorage.getItem('tileTitleCol'));
-    changeColorTilebg(localStorage.getItem('tileBGCol'));
-    gradeElems.length = 0;
-    weightedElems.length = 0;
-    classes.length = 0;
-    document.querySelectorAll('#app #coursesContainer .row').forEach(row => {
-      let gradeElem = row.querySelector('.col-md-2 h3.showGrade');
-      if (gradeElems.indexOf(gradeElem) === -1) {
-        gradeElems.push(gradeElem);
-      }
-      let weightedElem = row.querySelector('.col-md-3 a');
-      if (weightedElems.indexOf(weightedElem) === -1) {
-        weightedElems.push(weightedElem);
-      }
-      let classElem = row.querySelector('.col-md-3 a h3');
-      classes.push(classElem.textContent);
-    });
+        let grades = [];
 
-    if (gradeElems.length === 9) {
-      baseTile = document.getElementById('conduct');
-      observer.disconnect();
-      afterObserverCallback();
+        const classes = getElementFromXPath('//*[@id="coursesContainer"]', document);
+        for (const child of classes.children) {
+            let name = child.querySelector("h3").textContent;
+            name = name.split(" - ")[0];
+            let grade = child.querySelector(".showGrade").textContent;
+            grade = grade.trim();
+            let gradeLetter = null;
+            if (grade == "--") {
+                grade = "no grade value";
+            } else {
+                grade = parseFloat(grade);
+                gradeLetter = getGradeLetter(grade);
+            }
+            if (name.startsWith("AP") || name.endsWith("-H")) {
+                console.log(name + " is weighted with grade " + grade);
+                if(!isNaN(grade)) {
+                    grades.push(calcGPA(grade, true));
+                    let textElm = child.querySelector(".showGrade");
+                    textElm.textContent += "(" + gradeLetter + ")";
+                    textElm.dataset.modified = true;           
+                }
+            } else if (name == "Homeroom") {
+                console.log("thats homeroom ignore ts");
+            } else {
+                console.log(name + " is not weighted with grade " + grade);
+                if(!isNaN(grade)) {
+                    grades.push(calcGPA(grade, false));
+                    let textElm = child.querySelector(".showGrade");
+                    textElm.textContent += "(" + gradeLetter + ")";
+                    textElm.dataset.modified = true;
+                }
+            }
+
+
+
+        }
+
+        console.log(grades);
+        const GPA = calcCumGPA(grades);
+        console.log(GPA);
+
+        const gpaTile = createDIV(conductDIV, "GPA", GPA.toPrecision(3), "", "gpa");
+        row.appendChild(gpaTile);
     }
-  }
+});
 
-  function afterObserverCallback() {
-    appendGrades(gradeElems, gpas, weightedElems, grades);
-    createGPATile(baseTile, gpas);
-    createChangeTile(baseTile, grades, classes);
-    createStyleTile(baseTile);
-  }
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
 
-  function observeElement(selector) {
-    // Disconnect any existing observer
-    if (observer) {
-      observer.disconnect();
-    }
+// ACTIVE FUNCTIONS
 
-    const targetNode = document.documentElement;
 
-    // Create a new observer instance each time
-    observer = new MutationObserver((mutations, obs) => {
-      if (document.querySelector(selector)) {
-        main();
-      }
-    });
 
-    observer.observe(targetNode, {
-      childList: true,
-      subtree: true
-    });
-  }
+// HELPER FUNCTIONS
 
-
-  // SPA URL change handling
-  (function (history) {
-    const pushState = history.pushState;
-    const replaceState = history.replaceState;
-    history.pushState = function () {
-      const result = pushState.apply(history, arguments);
-      window.dispatchEvent(new Event('locationchange'));
-      return result;
-    };
-    history.replaceState = function () {
-      const result = replaceState.apply(history, arguments);
-      window.dispatchEvent(new Event('locationchange'));
-      return result;
-    };
-    window.addEventListener('popstate', function () {
-      window.dispatchEvent(new Event('locationchange'));
-    });
-  })(window.history);
-
-  window.addEventListener('locationchange', function () {
-    console.log("URL change detected");
-    observeElement('#app');
-  });
-
-  // Initial observer
-  observeElement('#app');
-})();
-
-/* <------------------- ACTION METHODDS ----------------> */
-
-// append grade letters to each grade
-function appendGrades(gradeElems, gpas, weightedElems, grades) {
-  for (let i = 0; i < gradeElems.length; i++) {
-    let elem = gradeElems[i];
-    let weightedElem = weightedElems[i];
-    let grade = parseFloat(elem.textContent);
-    grades.push(grade);
-    if (!isNaN(grade)) {
-      elem.textContent += (`(${getGradeLetter(grade)})`);
-      let classID = weightedElem.getAttribute('href');
-      let weighted = false;
-      classID = classID.replace('#academicclass/', '');
-      classID = classID.replace('/undefined/bulletinboard', '');
-      if (classID.substring(0, 3) == '976') {
-        weighted = true;
-      } else if (classID.substring(0, 3) == '977') {
-        weighted = false;
-      }
-      gpas.push(calcGPA(grade, weighted));
-      if (getGradeLetter(grade) === 'F') {
-        elem.style.color = "red";
-      }
-    }
-  }
-}
-
-// Create the GPA tile block
-function createGPATile(baseTile, gpas) {
-  const savedGPA = JSON.parse(localStorage.getItem('GPA'));
-  let actualGPA = Math.trunc(calcGPAAvg(gpas) * 100) / 100; 
-  localStorage.setItem('GPA', JSON.stringify(Math.trunc(calcGPAAvg(gpas) * 100) / 100));
-  let GPATile = baseTile.cloneNode(true);
-  GPATile.id = 'GPA';
-
-  let title = GPATile.querySelector('h2.bb-tile-header');
-  title.textContent = 'GPA';
-
-  let desc = GPATile.querySelector('h5');
-  desc.textContent = "Current Weighted GPA";
-
-  $(GPATile).find('.col-md-12 div a h1').unwrap();
-  let GPA = GPATile.querySelector('.col-md-12 h1');
-  GPA.textContent = actualGPA;
-  if (savedGPA.isNaN) {
-    GPA.textContent = "ERROR: Reload page";
-  }
-
-  let GPAChange = GPATile.querySelector('.col-md-12');
-  GPAChange.innerHTML = '';
-
-  let changedGPAText;
-  if (savedGPA > actualGPA) {
-    changedGPAText = `<span style="color: red;">↓${savedGPA}</span>`;
-  } else if (savedGPA < actualGPA) {
-    changedGPAText = `<span style="color: green;">↑${savedGPA}</span>`;
-  } else {
-    changedGPAText = ""
-  }
-
-  let CurrentGPA = document.createElement('h1');
-  CurrentGPA.innerHTML = actualGPA + changedGPAText;
-  GPAChange.appendChild(CurrentGPA);
-  
-  let text = document.createElement('p');
-  text.textContent = "Current Weighted GPA";
-  GPAChange.appendChild(text);
-
-
-
-  let dataAtt = GPATile.querySelector('.bb-tile-title');
-  dataAtt.setAttribute('data-target', '#GPACollapse');
-
-  let seperAtt = GPATile.querySelector('#conductCollapse');
-  seperAtt.setAttribute('id', 'GPACollapse');
-
-  baseTile.parentNode.appendChild(GPATile);
-}
-
-function createStyleTile(baseTile) {
-  let styleTile = baseTile.cloneNode(true);
-  styleTile.id = 'style';
-
-  let title = styleTile.querySelector('h2.bb-tile-header');
-  title.textContent = 'Colors';
-
-  $(styleTile).find(".col-md-12 div a h1").remove();
-  $(styleTile).find(".muted").remove();
-
-  let styleContainer = styleTile.querySelector('.col-md-12');
-  styleContainer.innerHTML = '';
-
-  let topBar = document.createElement('input');
-  topBar.type = 'color';
-  topBar.value = localStorage.getItem('topCol');
-  styleContainer.appendChild(topBar);
-
-
-  topBar.addEventListener('input', (event) => {
-    changeColorTopBar(event.target.value);
-  });
-
-  let middleBar = document.createElement('input');
-  middleBar.type = 'color';
-  middleBar.value = localStorage.getItem('midCol');
-  styleContainer.appendChild(middleBar);
-
-
-  middleBar.addEventListener('input', (event) => {
-    changeColorMiddleBar(event.target.value);
-  });
-  
-  let lowerBar = document.createElement('input');
-  lowerBar.type = 'color';
-  lowerBar.value = localStorage.getItem('lowCol');
-  styleContainer.appendChild(lowerBar);
-
-
-  lowerBar.addEventListener('input', (event) => {
-    changeColorLowerBar(event.target.value);
-  });
-
-  let body = document.createElement('input');
-  body.type = 'color';
-  body.value = localStorage.getItem('bodyCol');
-  styleContainer.appendChild(body);
-
-
-  body.addEventListener('input', (event) => {
-    changeColorBody(event.target.value);
-  });
-
-  let tileTitle = document.createElement('input');
-  tileTitle.type = 'color';
-  tileTitle.value = localStorage.getItem('tileTitleCol');
-  styleContainer.appendChild(tileTitle);
-
-
-  tileTitle.addEventListener('input', (event) => {
-    changeColorTileTitle(event.target.value);
-  });
-
-  let tileBG = document.createElement('input');
-  tileBG.type = 'color';
-  tileBG.value = localStorage.getItem('tileBGCol');
-  styleContainer.appendChild(tileBG);
-
-
-  tileBG.addEventListener('input', (event) => {
-    changeColorTilebg(event.target.value);
-  });
-
-  let resetButton = document.createElement('button');
-  resetButton.textContent = "Reset Colors";
-  resetButton.onclick = resetColors;
-  styleContainer.appendChild(resetButton);
-
-
-  let dataAtt = styleTile.querySelector('.bb-tile-title');
-  dataAtt.setAttribute('data-target', '#styleCollapse');
-
-  let seperAtt = styleTile.querySelector('#conductCollapse');
-  seperAtt.setAttribute('id', 'styleCollapse');
-
-  baseTile.parentNode.appendChild(styleTile);
-}
-
-
-// Create the Grade Change tile block
-function createChangeTile(baseTile, grades, classes) {
-  const savedGrades = JSON.parse(localStorage.getItem('grades'));
-  localStorage.setItem('grades', JSON.stringify(grades));
-  let GCTile = baseTile.cloneNode(true);
-  GCTile.id = 'GradeChange';
-
-  let title = GCTile.querySelector('h2.bb-tile-header');
-  title.textContent = 'Grade Changes';
-
-  let desc = GCTile.querySelector('h5');
-  desc.textContent = "No Grade Changes";
-
-  $(GCTile).find(".col-md-12 div a h1").wrap('<h5></h5>').parent().contents().unwrap();
-  $(GCTile).find(".col-md-12 div a h1").remove();
-
-  let GCContainer = GCTile.querySelector('.col-md-12');
-  GCContainer.innerHTML = '';
-
-  let gradeChangeTexts = [];
-
-  for (i = 0; i < savedGrades.length; i++) {
-    if (grades[i] > savedGrades[i]) {
-      gradeChangeTexts.push(`<span style="color: green;">${classes[i]}: ${savedGrades[i]} ↑ ${grades[i]}</span>`);
-    } else if (grades[i] < savedGrades[i]) {
-      gradeChangeTexts.push(`<span style="color: red;">${classes[i]}: ${savedGrades[i]} ↓ ${grades[i]}</span>`);
-    } else {
-      gradeChangeTexts.push(0);
-    }
-  }
-
-  if (gradeChangeTexts.every(text => text === 0)) {
-    let gradeElem = document.createElement('h5');
-    gradeElem.textContent = "No grade changes.";
-    GCContainer.appendChild(gradeElem);
-  } else {
-    for (const text of gradeChangeTexts) {
-      if (text != 0) {
-        let gradeElem = document.createElement('h5');
-        gradeElem.innerHTML = text;
-        GCContainer.appendChild(gradeElem);
-      }
-    }
-  }
-
-  let dataAtt = GCTile.querySelector('.bb-tile-title');
-  dataAtt.setAttribute('data-target', '#GCCollapse');
-
-  let seperAtt = GCTile.querySelector('#conductCollapse');
-  seperAtt.setAttribute('id', 'GCCollapse');
-
-  baseTile.parentNode.appendChild(GCTile);
-}
-
-/* extremly unoptimized color changer WILL FIX LATER PLEASE*/
-
-function changeColorTopBar(color) {
-  const top = document.querySelector('.pri-100-bgc');
-  top.style.setProperty('background-color', color, 'important');
-  localStorage.setItem('topCol', color);
-}
-
-function changeColorMiddleBar(color) {
-  const top = document.querySelector('.sec-15-bgc');
-  top.style.setProperty('background-color', color, 'important');
-  localStorage.setItem('midCol', color);
-}
-
-function changeColorLowerBar(color) {
-  const top = document.querySelector('.subnavbar');
-  top.style.setProperty('background-color', color, 'important');
-  localStorage.setItem('lowCol', color);
-}
-
-function changeColorBody(color) {
-  const top = document.querySelector('body');
-  top.style.setProperty('background-color', color, 'important');
-  localStorage.setItem('bodyCol', color);
-}
-
-function changeColorTileTitle(color) {
-  const top = document.querySelectorAll('.bb-tile .bb-tile-title');
-  top.forEach(el => {
-    el.style.setProperty('background-color', color, 'important');
-  })
-  localStorage.setItem('tileTitleCol', color);
-}
-
-function changeColorTilebg(color) {
-  const top = document.querySelectorAll('.bb-tile-content');
-  top.forEach(el => {
-    el.style.setProperty('background-color', color, 'important');
-  })
-  localStorage.setItem('tileBGCol', color);
-}
-
-function resetColors() {
-  changeColorTopBar('#004a97');
-  changeColorMiddleBar('#e8f3fc');
-  changeColorLowerBar('#e8f3fc');
-  changeColorBody('#eeeeef');
-  changeColorTileTitle('#ffffff');
-  changeColorTilebg('#ffffff');
-}
-/* <----------------- HELPER FUNCTIONS -----------------> */
-
-// convert grade number to grade letter
-function getGradeLetter(grade) {
-  switch (true) {
-    case (grade >= 97):
-      return 'A+';
-    case (grade >= 95):
-      return 'A';
-    case (grade >= 93):
-      return 'A-';
-    case (grade >= 90):
-      return 'B+';
-    case (grade >= 87):
-      return 'B';
-    case (grade >= 85):
-      return 'B-';
-    case (grade >= 83):
-      return 'C+';
-    case (grade >= 79):
-      return 'C';
-    case (grade >= 76):
-      return 'C-';
-    case (grade >= 74):
-      return 'D+';
-    case (grade >= 72):
-      return 'D';
-    case (grade >= 70):
-      return 'D-';
-    default:
-      return 'F';
-  }
-}
-
-// take a class and weightedness and spit a gpa number out
+// calculate GPA per class
+// bro tf is this GPA calculation maybe idk but this seems kinda stupid
 function calcGPA(grade, Weighted) {
-  var gpa;
-  if (grade < 70) {
-    return 1;
-  } else if (grade >= 70 && grade < 76) {
-    // point in value bracket kinda like taxes but for grades
-    var piv = grade - 70;
-    gpa = (((2.00 - 1.00) / 6) * piv) + 1.00;
-  } else if (grade >= 76 && grade < 85) {
-    var piv = grade - 76;
-    gpa = (((3.00 - 2.00) / 9) * piv) + 2.00;
-  } else if (grade >= 85 && grade < 93) {
-    var piv = grade - 85;
-    gpa = (((4.00 - 3.00) / 8) * piv) + 3.00;
-  } else if (grade >= 93) {
-    var piv = grade - 93;
-    gpa = (((4.86 - 4.00) / 7) * piv) + 4.00;
-  }
+    var gpa;
+    if (grade >= 70 && grade < 76) {
+        // point in value bracket kinda like taxes but for grades
+        var piv = grade - 70;
+        gpa = (((2.00 - 1.00) / 6) * piv) + 1.00;
+    } else if (grade >= 76 && grade < 85) {
+        var piv = grade - 76;
+        gpa = (((3.00 - 2.00) / 9) * piv) + 2.00;
+    } else if (grade >= 85 && grade < 93) {
+        var piv = grade - 85;
+        gpa = (((4.00 - 3.00) / 8) * piv) + 3.00;
+    } else if (grade >= 93) {
+        var piv = grade - 93;
+        gpa = (((4.86 - 4.00) / 7) * piv) + 4.00;
+    }
 
-  if (Weighted) {
-    gpa += 1;
-  }
-  console.log(gpa);
+    if (Weighted) {
+        gpa += 1;
+    }
 
-  return gpa;
+    return gpa;
 }
 
-// Calculates the GPA from the GPs
-function calcGPAAvg(gpas) {
-  let sum = 0;
-  for (const gpa of gpas) {
-    console.log(gpa);
-    sum += gpa;
-    console.log(sum);
-  }
-  sum /= gpas.length;
+// calculate cumulative GPA
+// i don't even know how this works if im even doing it correctly but i guess ill see
+function calcCumGPA(grades) {
+    var count = grades.length;
+    var total = 0;
+    for (i=0; i<grades.length; i++) {
+        total += grades[i];
+    }
 
-  console.log(sum);
-  return sum;
+    var cumGPA = total / count;
+    return cumGPA;
+}
+
+// thing to get the letter grade cause thats important cause oncampus+ used it so i gotta
+// I also forgot the grading scale for - and + used in oc+ so im just gonna have to guess
+// also who tf getting less than a 70 in a class :skull:
+function getGradeLetter(grade) {
+    switch (true) {
+        case (grade >= 97):
+            return 'A+';
+        case (grade >= 95):
+            return 'A';
+        case (grade >= 93):
+            return 'A-';
+        case (grade >= 90):
+            return 'B+';
+        case (grade >= 87):
+            return 'B';
+        case (grade >= 85):
+            return 'B-';
+        case (grade >= 83):
+            return 'C+';
+        case (grade >= 79):
+            return 'C';
+        case (grade >= 76):
+            return 'C-';
+        case (grade >= 74):
+            return 'D+';
+        case (grade >= 72):
+            return 'D';        
+        case (grade >= 70):
+            return 'D-';
+        default:
+            return 'F';
+    }
+}
+
+function createDIV(target, title, maintext, subtext, intname) {
+    const cleanedDIV = target.cloneNode(true);
+    cleanedDIV.id = intname;
+    const Title = cleanedDIV.querySelector(".bb-tile-header");
+    Title.textContent = title;
+    const mainTextParent = cleanedDIV.querySelector("a");
+    const mainText = mainTextParent.querySelector("h1");
+    mainTextParent.parentNode.insertBefore(mainText, mainTextParent);
+    mainTextParent.remove();
+    mainText.textContent = maintext;
+    const subText = cleanedDIV.querySelector(".muted h5")
+    subText.textContent = subtext;
+    const collapseOBJ = cleanedDIV.querySelector("#conductCollapse");
+    collapseOBJ.id = intname + "Collapse";
+    const otherCollapseOBJ = cleanedDIV.querySelector(".bb-tile-title");
+    otherCollapseOBJ.setAttribute("data-target", "#" + intname + "Collapse");
+    return cleanedDIV;
+}
+
+function getElementFromXPath(XPath, document) {
+    const obj = document.evaluate(
+        XPath,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null
+    ).singleNodeValue;
+
+    return obj;
+}
+
+function createBanner(text, document) {
+    const banner = document.createElement("div");
+    banner.textContent = text;
+    banner.style = `
+        position: fixed;
+        top: 0;
+        width: 100%;
+        padding: 5px;
+        background: black;
+        color: white;
+        text-align: center;
+        z-index: 9999;
+    `;
+
+    document.body.appendChild(banner);
 }
---
+++ popup/popup.js
@@ -0,0 +1,10 @@
+// these buttons do jackshit yet, so WIP
+document.getElementById("WIPc").addEventListener("click", () => {
+    alert("WIP");
+})
+document.getElementById("WIPs").addEventListener("click", () => {
+    alert("WIP");
+})
+document.getElementById("WIPg").addEventListener("click", () => {
+    alert("WIP");
+})
