f5e702a40dddf4de18f9852c8d180352d654e90e
SuperSteve9 <bgepictest@gmail.com>
2025-08-20 00:38:32 -0500

    installer
    try #1

--- run.bat
+++ run.bat
@@ -7,23 +7,23 @@ if not exist mySFHS (
 cd mySFHS
 
 curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/content.js" --output content.js
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/gamecode.js" --output gamecode.js
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/init-buffers.js" --output init-buffers.js
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/draw-scene.js" --output draw-scene.js
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/jquery-3.7.1.min.js" --output jquery-3.7.1.min.js
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/gl-matrix-min.js" --output gl-matrix-min.js
 curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/manifest.json" --output manifest.json
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/popup.html" --output popup.html
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/game.html" --output game.html
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/cubetexture.png" --output cubetexture.png
 
 if not exist icons (
   mkdir icons
 )
-cd icons
-curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/icons/128.png" --output 128.png
+cd img
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/img/128.png" --output 128.png
 
 cd ..
+
+if not exist popup (
+  mkdir popup
+)
+cd popup
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/popup/popup.html" --output popup.html
+curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/popup/popup.js" --output popup.js
+
 cd ..
 
 curl "https://raw.githubusercontent.com/SuperSteve9/MySFHSplus/refs/heads/main/run.bat" --output run.bat
