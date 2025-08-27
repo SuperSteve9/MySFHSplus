a80cb3fa086d1f0b477f16e2f61ae4d217a60445
SuperSteve9 <80017099+SuperSteve9@users.noreply.github.com>
2025-08-27 07:37:21 -0500

    Fixed precision thingy

--- content.js
+++ content.js
@@ -102,9 +102,9 @@ function runOnDomChange() {
                 for (i = 0; i < grades.length; i++) {
                     if (grades[i] != oldGrades[i]) {
                         if (grades[i] >= oldGrades[i]) {
-                            gradeChanges += classes[i] + ": " + oldGrades[i].toPrecision(3) + " ↑ " + grades[i].toPrecision(3) + "\n";
+                            gradeChanges += classes[i] + ": " + oldGrades[i].toFixed(3) + " ↑ " + grades[i].toFixed(3) + "\n";
                         } else {
-                            gradeChanges += classes[i] + ": " + oldGrades[i].toPrecision(3) + " ↓ " + grades[i].toPrecision(3) + "\n";
+                            gradeChanges += classes[i] + ": " + oldGrades[i].toFixed(3) + " ↓ " + grades[i].toFixed(3) + "\n";
                         }
                     }
                 }
@@ -301,3 +301,4 @@ function arraysEqual(a, b) {
     }
     return true;
 }
+
